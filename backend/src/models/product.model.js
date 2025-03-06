500b03cd7c15040496fee330bc1f99b87bfec282
vikas singh <vikas7490@gmail.com>
2025-03-06 16:26:28 +0530

    model updated

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const detailSchema = new mongoose.Schema({
   productType: {
@@ -74,4 +75,5 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+productSchema.plugin(mongooseAggregatePaginate);
 export const Product = mongoose.model("Product", productSchema);
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -6,12 +8,14 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       lowercase: true,
+      trim: true,
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
+      trim: true,
     },
     password: {
       type: String,
@@ -20,9 +24,24 @@ const userSchema = new mongoose.Schema(
     address: {
       type: String,
       required: false,
+      trim: true,
+    },
+    refreshToken: {
+      type: String,
     },
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
